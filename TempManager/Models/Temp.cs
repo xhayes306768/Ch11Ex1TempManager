e80e6020a981a218b53813456352747b77b0edbf
Xavier Hayes <xavier.hayes@southeasttech.edu>
2024-03-03 22:16:18 -0600

    Added data validation to the Temp domain model ,

--- TempManager/Models/Temp.cs
+++ TempManager/Models/Temp.cs
@@ -1,12 +1,21 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TempManager.Models
 {
     public class Temp
     {
         public int Id { get; set; }
+
+        [Required]
         public DateTime? Date { get; set; }
+
+        [Required]
+        [Range(-200, 200, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public double? Low { get; set; }
+
+        [Required]
+        [Range(-200, 200, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public double? High { get; set; }
     }
 }
